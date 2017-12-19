ccbeab6a531982794722f24b06b69eb220382662
sirius <sirius567@hotmail.com>
2017-12-19 18:31:31 +0100

    our first commit

---
+++ first_code.R
@@ -0,0 +1,31 @@
+# Vectors
+# Matrices
+# Data Frames
+# Lists
+
+
+
+# IN THIS SESSION WE'LL LEARN ABOUT TYPES OF 
+# OBJECTS AND DATA CONTEINERS IN R
+
+###########################################################
+# MATRIX
+
+# is the most basic data structure in R, allowing only for 
+# numeric variables.
+# Besides, all variables must be of the same length
+
+# matrix operations are very optimised though
+
+# let's create a matrix
+
+m<-matrix(seq(1,12),nrow=3,ncol=4)
+n<-matrix(seq(1,12),nrow=3,byrow=T)
+
+m
+n
+
+dim(m)
+
+colnames(m)<-paste0(rep('col',4),seq(1,4))
+rownames(m)<-paste0(rep('row',3),seq(1,3))
